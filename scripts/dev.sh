9ea9b2102371bc2738e847cb889db310868b4280
aryan02420 <aryan02420@gmail.com>
2025-02-02 23:53:59 +0530

    👷 chore: manually bundle extension because blender wont pack subdirectories

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,4 +4,6 @@
 # This script watch for changes in the addon directory,
 # and automatically builds the addon using blender.
 
-find ./addons/io_mesh_bombsquad/ | entr -s 'blender --command extension build --source-dir $(realpath ./addons/io_mesh_bombsquad/) --output-dir $(realpath ./addons/io_mesh_bombsquad/)'
+# find ./addons/io_mesh_bombsquad/ | entr -s 'blender --command extension build --source-dir $(realpath ./addons/io_mesh_bombsquad/) --output-dir $(realpath ./addons/io_mesh_bombsquad/)'
+
+find ./addons/io_mesh_bombsquad/ | entr -cs 'date --rfc-email; (cd ./addons/io_mesh_bombsquad/ && zip -r io_mesh_bombsquad-3.0.0.zip . -x *.zip -x *.md)'
